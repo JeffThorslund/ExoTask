3f4b754976e001b83c532a5fafb67920c799a585
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-06-20 11:26:25 -0400

    cleaned Note

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,40 +1,41 @@
 import React from "react";
-import Tasking from "./Tasking Components/Tasking";
-import Title from "./Tasking Components/Title";
-import { Card } from "react-bootstrap";
-import "./Note.scss";
-import { MdClose } from "react-icons/md";
 import PropTypes from "prop-types";
 
-const Note = props => {
-  const deleteNote = e => {
-    
-    props.deleteNote(e.currentTarget.id);
-  };
+import Tasking from "./Tasking Components/Tasking";
+import Title from "./Tasking Components/Title";
+
+import { MdClose } from "react-icons/md";
+import { Card } from "react-bootstrap";
+
+import "./Note.scss";
+
+const Note = (props) => {
+  const { deleteNote, id } = props;
 
   return (
-    
-      <Card style={{ width: "18rem" }} id="card-wrapper">
-        <Card.Body>
-          <div id="flex-container">
-            <Title />
-            <div><MdClose onClick={deleteNote} id={props.id} /></div>
+    <Card style={{ width: "18rem" }} id="card-wrapper">
+      <Card.Body>
+        <div id="flex-container">
+          <Title />
+          <div>
+            <MdClose
+              onClick={(e) => {
+                deleteNote(e.currentTarget.id);
+              }}
+              id={id}
+            />
           </div>
-
-          {/*index:{props.index} <br />
-          id:{props.id}*/}
-
-          <Tasking />
-        </Card.Body>
-      </Card>
-    
+        </div>
+        <Tasking />
+      </Card.Body>
+    </Card>
   );
 };
 
 Note.propTypes = {
-  id: PropTypes.number,
   deleteNote: PropTypes.func,
-  index: PropTypes.number
+  index: PropTypes.number,
+  id: PropTypes.number,
 };
 
 export default Note;
